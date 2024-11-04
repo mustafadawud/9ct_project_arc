14ff3c08b6eff631087b1a8ac77aa548841dd611
mustafadawud <166460180+mustafadawud@users.noreply.github.com>
2024-11-05 10:46:28 +1100

    fixed the 2 player so they dont fly out of map
    22.38

--- First Game/scripts/multiplayer_manger.gd
+++ First Game/scripts/multiplayer_manger.gd
@@ -12,7 +12,6 @@ func become_host():
 	
 	_player_spawn_node = get_tree().get_current_scene().get_node("Players")
 	
-	
 	var  server_peer = ENetMultiplayerPeer.new()
 	server_peer.create_server(SERVER_PORT)
 	
@@ -20,16 +19,25 @@ func become_host():
 	
 	multiplayer.peer_connected.connect(_add_player_to_game)
 	multiplayer.peer_disconnected.connect(_add_player_to_game)
+	
+	_remove_single_player()
+	
+	_add_player_to_game(1)
+	
+	
+	
 func join_as_player_2():
-	print("player 2 joining")
-
-func _add_player_to_game(id: int):
-	print("Player %s joined the game!" % id)
+	print("Player 2 joining")
 	
 	var client_peer = ENetMultiplayerPeer.new()
-	client_peer.create_cilentS(SERVER_IP, SERVER_PORT)
+	client_peer.create_cilent(SERVER_IP, SERVER_PORT)
 	
 	multiplayer.multiplayer_peer = client_peer
+	
+	_remove_single_player()
+	
+func _add_player_to_game(id: int):
+	print("Player %s joined the game!" % id)
 	
 	var player_to_add = multiplayer_scene.instantiate()
 	player_to_add.player_id = id
@@ -39,3 +47,13 @@ func _add_player_to_game(id: int):
 	
 func _del_player(id: int):
 	print("Player %s left the game!" % id)
+	if not _player_spawn_node.has_node(str(id)):
+		return
+	_player_spawn_node.get_node(str(id)).queue_free()
+	
+	
+	
+func _remove_single_player():
+	print("Remove single player")
+	var player_to_remove = get_tree().get_current_scene().get_node("Player")
+	player_to_remove.queue_free()
